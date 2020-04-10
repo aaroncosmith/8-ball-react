18248127e1fca27f68273608c531c415d18c64fb
Aaron Smith <aaroncosmith@gmail.com>
2020-04-10 13:03:01 -0400

    added component to save old questions to

--- src/components/Eight.jsx
+++ src/components/Eight.jsx
@@ -1,9 +1,24 @@
 import React, { Component } from 'react';
+import EightCard from './EightCard';
 
 class Eight extends Component {
-    state = {
-        question: "Are you welcomed to this react app?",
-        answer: "What is the answer?"
+    constructor(props) {
+        super(props);
+        this.state = {
+            question: "Are you welcomed to this react app?",
+            answer: "What is the answer?",
+            previous: [
+                {
+                    question: "Old questions go here",
+                    answer: "You can't change my mind!"
+                }
+            ]
+        }
+    }
+    
+    addEightCard = () => {
+        const newEightList = [ ...this.state.previous, { question: this.state.question, answer: this.state.answer } ]
+        this.setState({previous: newEightList, question: '', answer:''})
     }
     async componentDidMount() {
         const { question } = this.state;
@@ -20,9 +35,24 @@ class Eight extends Component {
     render() {
         const { question, answer } = this.state;
         return (
-            <div>
-                <h1>{question}</h1>
-                <h2>{answer}</h2>
+            <div className="eight">
+                <div className="dev">
+                    <h1>{question}</h1>
+                    <h2>{answer}</h2>
+                </div>
+                <div className="control-nav">
+                    <input onChange={this.changeTitle} value={this.state.title} />
+                    <input onChange={this.changeDescription} value={this.state.description} />
+                    <button onClick={this.addTodo}/>
+                </div>
+                <div className="eight-body">
+                    {
+                        this.state.previous.map((eaEight) => (
+                            <EightCard eightcard={eaEight}/>
+                        ))
+                    }
+                </div>
+
             </div>
         )
     }
---
+++ src/components/EightCard.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const EightCard = ({ eightcard }) => {
+    return (
+        <div className="eight-card">
+            <div className="question">
+                test q
+            </div>
+            <div className="answer">
+                test a
+            </div>
+        </div>
+    )
+}
+
+export default EightCard;
